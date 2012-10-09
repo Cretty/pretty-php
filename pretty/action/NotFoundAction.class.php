a8cb3afb5aa77399e4229690641ebf160d1273ab
Shawn <sflyknife@gmail.com>
2012-10-09 14:48:12 +0800

    fix notfound in windows

--- pretty/action/NotFoundAction.class.php
+++ pretty/action/NotFoundAction.class.php
@@ -5,21 +5,26 @@ namespace net\shawn_huang\pretty\action;
 use \net\shawn_huang\pretty as p;
 
 class NotFoundAction extends p\Action {
-    private $data;
+    private $myData;
     protected function run() {
         $log = p\Pretty::getLog();
         foreach($log as $k => $v) {
             $arr = explode(':', $k);
             switch (count($arr)) {
                 case 2:
-                    $this->data[$arr[0]][$arr[1]] = $v;
+                    $this->myData[$arr[0]][$arr[1]] = $v;
                     break;
+                case 3:
+                    if (PATH_SEPARATOR !== ':') {
+                        $this->myData[$arr[0]][$arr[1] . $arr[2]] = $v;
+                        break;
+                    }
                 default:
-                    $this->data[$k] = $v;
+                    $this->myData[$k] = $v;
                     break;
             }
         }
-        $this->setData($this->data);
+        $this->setData($this->myData);
         $this->setView('nofound', 'Debug');
     }
 
