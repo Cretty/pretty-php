f0d90dc043347eabfe21c28b16d7e5540e9ed51e
Shawn <sflyknife@gmail.com>
2012-10-09 14:41:44 +0800

    fix debug action on php5.3

--- pretty/action/NotFoundAction.class.php
+++ pretty/action/NotFoundAction.class.php
@@ -5,21 +5,21 @@ namespace net\shawn_huang\pretty\action;
 use \net\shawn_huang\pretty as p;
 
 class NotFoundAction extends p\Action {
-    private $data;
+    private $myData;
     protected function run() {
         $log = p\Pretty::getLog();
         foreach($log as $k => $v) {
             $arr = explode(':', $k);
             switch (count($arr)) {
                 case 2:
-                    $this->data[$arr[0]][$arr[1]] = $v;
+                    $this->myData[$arr[0]][$arr[1]] = $v;
                     break;
                 default:
-                    $this->data[$k] = $v;
+                    $this->myData[$k] = $v;
                     break;
             }
         }
-        $this->setData($this->data);
+        $this->setData($this->myData);
         $this->setView('nofound', 'Debug');
     }
 
